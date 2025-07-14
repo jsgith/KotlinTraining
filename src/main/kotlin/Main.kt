348b1995b79b72e61dd8e58faa186b0443e5b536
joaosantos <joao_gens@hotmail.com>
2025-07-14 12:04:19 +0100

    try git commands - third commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,4 +2,5 @@ fun main(args: Array<String>) {
     println("Hello World!")
     // first commit to try git commands- rewording
     // second commit to try git commands
+    // third commit to try git commands
 }
